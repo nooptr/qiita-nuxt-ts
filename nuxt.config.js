b0ed805ce11e398fbd6de7b0d7b0e2deddf1e464
Nooptr <nooptr@techap.xyz>
2019-06-29 00:03:41 +0900

    first commit for nuxt-express-typescript-boostrap

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+"use strict";
+exports.__esModule = true;
+var pkg = require('./package');
+var config = {
+    mode: 'spa',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+    /*
+    ** Global CSS
+    */
+    css: [],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://bootstrap-vue.js.org/docs/
+        'bootstrap-vue/nuxt',
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/eslint-module'
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {},
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend: function (config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                if (!config.module)
+                    return;
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
+    }
+};
+exports["default"] = config;
