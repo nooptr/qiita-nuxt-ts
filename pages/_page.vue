4b225dc4b265c5cf4cfaa85a6857904a22bc58bf
Nooptr <nooptr@techap.xyz>
2019-06-30 23:52:24 +0900

    Refactoring page

--- pages/_page.vue
+++ pages/_page.vue
@@ -3,21 +3,13 @@
     <div>
       <h3>Nuxt.js のタグが付けられた投稿一覧</h3>
       <pagination :page="page"></pagination>
-      <transition :name="transition" mode="out-in">
-        <div :key="displayedPage" class="news-list">
-          <transition-group tag="ul" name="item">
-            <item v-for="item in items" :key="item.id" :item="item"/>
-          </transition-group>
-        </div>
-      </transition>
+      <item v-for="item in items" :key="item.id" :item="item"/>
     </div>
   </section>
 </template>
 
 <script lang='ts'>
-import { Component, Watch, Vue } from 'vue-property-decorator'
-import { Transition } from '@nuxt/vue-app'
-import { Route } from 'vue-router'
+import { Component, Vue } from 'vue-property-decorator'
 import Pagination from '../components/pagination.vue'
 import Item from '../components/item.vue'
 
@@ -25,22 +17,10 @@ import Item from '../components/item.vue'
   components: {
     Pagination,
     Item
-  },
-  asyncData({ route }) {
-    return {
-      displayedPage: Number(route.params.page) || 1
-    }
   }
 })
 
 export default class Index extends Vue {
-  displayedPage: number = 1
-  transition:
-    | null
-    | string
-    | Transition
-    | ((to: Route, from: Route) => string) = 'slide-right'
-
   get items() {
     return this.$store.state.items
   }
@@ -51,21 +31,15 @@ export default class Index extends Vue {
   }
 
   mounted() {
-    this.pageChanged(this.page)
+    this.pageChanged()
   }
 
-  @Watch('page')
-  pageChanged(to, from = -1) {
+  pageChanged() {
     this.$store
       .dispatch('FETCH_FEED', {
         page: this.page
       })
       .catch(() => {})
-
-    this.transition =
-      from === -1 ? null : to > from ? 'slide-left' : 'slide-right'
-
-    this.displayedPage = to
   }
 }
 </script>
